b60d0a8c3b54f68e8adc2e91c16de92daa0e4c2a
pgbeklui <gbekluip@sheridancollege.ca>
2023-03-22 12:35:31 -0400

    First Commit For Basis of Application

---
+++ index.js
@@ -0,0 +1,39 @@
+//Backend Server where API Request happen
+//Am express server, Which will handle API requests coming in and will respond with a json object. It will use a body parser aswell as cors
+const OpenAI = require('openai');
+const { Configuration, OpenAIApi } = OpenAI;
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port = 3001;
+
+const configuration = new Configuration({
+    organization: "org-rvHqBn8c3I9EGi7VVrpXvSk0",
+    apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/', async (req, res) => {
+    const { message } = req.body;
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `${message}`,
+        max_tokens: 10,
+        temperature: 0,
+      });
+    console.log(response.data)
+        if(response.data.choices[0].text){
+                res.json({ message: response.data.choices[0].text}) 
+        }
+    
+});
+
+app.listen(port, () =>{
+    console.log('Example app listening at http://localhost:3001');
+});
